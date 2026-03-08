be626729fa373d681ca9578447903222729d9778
aallaniharikakiran3 <aallaniharikakiran3@gmail.com>
2026-03-08 13:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.title("🔍 Image Classification")
+st.title("Image Classification App")
 
-st.write("Upload an image to get prediction from the trained model.")
+st.write("Upload an image to classify.")
 
-# Load model
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model("face_recognition_model.h5")
@@ -15,31 +14,22 @@ def load_model():
 
 model = load_model()
 
-# Image preprocessing
 def preprocess_image(image):
-
-    img = image.resize((224,224))
-    img = np.array(img)/255.0
+    image = image.resize((224,224))
+    img = np.array(image)/255.0
     img = np.expand_dims(img, axis=0)
-
     return img
 
-# Upload image
 uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file).convert("RGB")
-
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     img = preprocess_image(image)
 
     prediction = model.predict(img)
-
-    st.subheader("Prediction")
-
-    st.write(prediction)
 
     class_id = np.argmax(prediction)
 
