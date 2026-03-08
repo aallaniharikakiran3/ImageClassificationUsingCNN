59bc8191a02c14896d7d48d1529f7460462de9b9
aallaniharikakiran3 <aallaniharikakiran3@gmail.com>
2026-03-08 13:19:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from PIL import Image
 
 st.title("Image Classification App")
 
-st.write("Upload an image to classify.")
-
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model("face_recognition_model.h5")
@@ -16,7 +14,7 @@ model = load_model()
 
 def preprocess_image(image):
     image = image.resize((224,224))
-    img = np.array(image)/255.0
+    img = np.array(image) / 255.0
     img = np.expand_dims(img, axis=0)
     return img
 
@@ -25,7 +23,7 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image)
 
     img = preprocess_image(image)
 
